90932e957be2c011e5683f8616fdfcb74ddeec9b
BogacSabuncu <bsabuncu@mac.com>
2019-05-31 13:18:44 -0400

    fixed the frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,46 @@
-$(document).ready(function() {
-    
-   $(document).on("click", ".buy-button", handleBuy);
+$(document).ready(function () {
 
-    function createCard(product){
+    $(document).on("click", ".buy-button", handleBuy);
 
-        const card = $(`<div class="card m-3" style="width: 18rem; display:inline-block;">
-        <img src="${product.product_image}" class="card-img-top" alt="#">
-        <div class="card-body">
-          <h5 class="card-title">${product.product_name}</h5>
-          <p class="card-text">${product.department_name}</p>
-          <h6 class="card-text">${product.price}$</h6>
-          <p class="card-text">${product.stock_quantity} Left in stock!</p>
-          <button class="btn btn-primary buy-button" attr-id="${product.id}">Buy Now!</button>
-        </div>
-      </div>`)
+    function createCard(product) {
 
-        return card
+        if (product.stock_quantity <= 0) {
+            const card = $(`<div class="card m-4" style="width: 18rem; display:inline-block;">
+            <img src="${product.product_image}" class="card-img-top" alt="#">
+            <div class="card-body">
+              <h5 class="card-title">${product.product_name}</h5>
+              <p class="card-text">${product.department_name}</p>
+              <h6 class="price">$${product.price}</h6>
+              <p class="card-text">Sorry we are out of Stock!</p>
+            </div>
+              
+        <button class="btn-secondary buy-button" attr-id="${product.id}" disabled>Buy Now!</button >
+          </div>`)
+
+            return card
+        }
+        else {
+            const card = $(`<div class="card m-4" style="width: 18rem; display:inline-block;">
+            <img src="${product.product_image}" class="card-img-top" alt="#">
+            <div class="card-body">
+              <h5 class="card-title">${product.product_name}</h5>
+              <p class="card-text">${product.department_name}</p>
+              <h6 class="price">${product.price}$</h6>
+              <p class="card-text">${product.stock_quantity} Left in stock!</p>
+            </div>
+            <button class="buy-button" attr-id="${product.id}">Buy Now!</button>
+          </div>`)
+
+
+            return card
+        }
+
     }
 
-    function getProducts(){
-        $.get("/api/products", function(products){
+    function getProducts() {
+        $.get("/api/products", function (products) {
             console.log(products);
-        
+
             let productCard = $("#product-container");
 
             products.forEach(product => {
@@ -33,22 +52,16 @@ $(document).ready(function() {
     }
 
 
-    function handleBuy(){
+    function handleBuy() {
         let productId = $(this).attr("attr-id");
 
         $.ajax({
             method: "PUT",
-            url:`/api/buyitem/${productId}`
-        }).then(function(){
+            url: `/api/buyitem/${productId}`
+        }).then(function () {
             location.reload();
         });
     }
-
-    // $(".buy-button").click(function() {
-    //     console.log("here");
-
-    //     console.log($(this).attr("attr-id")); 
-    // });
 
     getProducts();
 });
